7815f92d9cd5bda9bcbd3da41deef2ce8f74a90a
Stian Skulstad <sti.sku@gmail.com>
2019-06-08 13:48:06 +0200

    Trying to make a initial menu. Should work at this point.

--- watch match WatchKit Extension/InterfaceController.swift
+++ watch match WatchKit Extension/InterfaceController.swift
@@ -12,9 +12,13 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var tableView: WKInterfaceTable!
+    
+    let tableData = ["One", "Two", "Three", "Four", "Five", "Six"]
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+        loadTableData()
         // Configure interface objects here.
     }
     
@@ -26,6 +30,18 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    private func loadTableData() {
+        
+        tableView.setNumberOfRows(tableData.count, withRowType: "RowController")
+        
+        for (index, rowModel) in tableData.enumerated() {
+            
+            if let rowController = tableView.rowController(at: index) as? RowController {
+                rowController.rowLabel.setText(rowModel)
+            }
+        }
     }
 
 }
---
+++ watch match WatchKit Extension/RowController.swift
@@ -0,0 +1,17 @@
+//
+//  RowController.swift
+//  watch match WatchKit Extension
+//
+//  Created by Stian on 08/06/2019.
+//  Copyright © 2019 Stian Skulstad. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class RowController:NSObject {
+    
+    @IBOutlet weak var rowLabel: WKInterfaceLabel!
+    
+    
+}
